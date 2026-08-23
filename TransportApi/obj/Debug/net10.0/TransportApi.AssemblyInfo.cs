2138dce709cf042cd8b6b33490889369e7cda5b6
Marameslo <161720226+Marameslo@users.noreply.github.com>
2026-08-23 18:26:57 +0200

    Add Dockerfile and resolve merge conflicts
    Add Dockerfile for containerizing the TransportApi application and resolve merge conflicts in TransportApi.csproj. Updated project dependencies to use Microsoft.EntityFrameworkCore.Relational 9.0.2 and Microsoft.OpenApi 2.3.12. Also cleaned up build artifacts from bin and obj directories.

--- TransportApi/obj/Debug/net10.0/TransportApi.AssemblyInfo.cs
+++ TransportApi/obj/Debug/net10.0/TransportApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransportApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ba22514a10541333aca24220fa6d41f7494a37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5afe82729816a96841c64601103f35edf6ca0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransportApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransportApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
